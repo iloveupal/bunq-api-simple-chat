bbb0b38b36f58f27c5a4c57a98262f2adc71b1bd
Danil Sorokin <d.sorokin.msk@icloud.com>
2019-04-04 19:05:17 +0200

    boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            {
+                test: /\.(png|jpg|gif|svg)$/i,
+                loader: 'url-loader',
+            },
+        ],
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    resolve: {
+        alias: {
+            Components: path.resolve(__dirname, 'src/components/'),
+            Framework: path.resolve(__dirname, 'src/framework'),
+            Modules: path.resolve(__dirname, 'src/modules'),
+            Config: path.resolve(__dirname, 'src/config'),
+            Utils: path.resolve(__dirname, 'src/utils'),
+        }
+    },
+};
